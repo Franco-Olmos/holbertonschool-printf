d4fc439bfe86602454577250d807d1bf183f0cf5
Franco-Olmos <5749@holbertonstudents.com>
2022-11-07 12:51:46 -0800

    readme mod

--- main.h
+++ main.h
@@ -1,27 +1,47 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdarg.h>
+
+/*libraries*/
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
 /**
  * struct vtype - struct vtype
  * @tp: tp
  * @f: function
  *
  */
+
+/*functions*/
+
 typedef struct vtype
 {
 	char tp;
 	void (*f)();
 } vtype_t;
+
 int _printf(const char *format, ...);
+
 int _putchar(va_list args, int mode);
+
 int stringiterator(va_list args, int mode);
+
 int ntostring(va_list args, int mode);
+
 int untostring(va_list ap, int base);
+
 int singlewrite(char c);
+
 int revstring(va_list ap, int mode);
+
 char *rot13(char *s);
+
 char *_strdup(char *str);
+
 int strot(va_list ap, int mode);
+
 ftype func_finder(char c);
+
 #endif 
